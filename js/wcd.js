dc4c5f0608eea53b1406015529c0e7ab0eaa80c3
wcdone <wcd.one@gmail.com>
2018-11-16 16:40:27 +0700

    bootstrap 2

---
+++ js/wcd.js
@@ -0,0 +1,13 @@
+
+
+    function validate(form) {
+      var elems = form.elements;
+
+      if (!elems.from.value) {
+       // showError(elems.from.parentNode, ' Укажите от кого.');
+       allert("sdfsdv");
+      }
+
+
+
+    }
