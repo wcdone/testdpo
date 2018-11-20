576f10185f227c78d66a4117f5b6c0002cb319fb
wcdone <wcd.one@gmail.com>
2018-11-20 11:37:42 +0700

    fix

--- js/wcd.js
+++ js/wcd.js
@@ -3,7 +3,7 @@
     function validate() {
       
 
-$.get( "file:///C:/src/wcdone.git/result.php", function( data ) {
+$.get( "result.php", function( data ) {
   var text = data;
   $("#result").html("<p>Новье!"+text+"</p>")
 });
