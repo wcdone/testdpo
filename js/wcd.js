8c93fa806c998249aba3f8df32c023ccee592352
wcd.one@gmail.com <wcd.one@gmail.com>
2021-05-10 08:01:42 +0700

    Change validatioon logic

--- js/wcd.js
+++ js/wcd.js
@@ -1,12 +1,46 @@
 
+function errors() {
 
-    function validate() {
+  $("#result").html("");
+  var value = $('#example-text-input').val();
+  $("#result").html("К сожалению документы №"+value+" не найдены " +"");
+
+}
+
+
+
+function validate() {
       
+$("#result").html("");
+  var value = $('#example-text-input').val();
+if(value.length == 0)
+  return false;
+  var regex = /(\d{4,10})/;
+
+  var match = regex.exec(value);
+  
+  if(match!=null){
+      $.get( "result_.php?code="+match[1], function( data ) {
+          var text = data;
+           if(text.indexOf("false")==-1){
+            var jarr = JSON.parse(text);
+             $("#result").html("Документы №"+jarr[0]+" выданы " + jarr[1]+" от "+ jarr[2] +".");
+             //$("#result").html("Удостоверение №"+match[1]+" выдано " + text +".");
+          }
+          else{
+           errors();
+          }
+         
 
 
-$.get( "https://wcdone.github.io/result.php", function( data ) {
-  var text = data;
-  $("#result").html("<p>Удостоверение №"+text+"валидно</p>")
-});
+      });
 
     }
+    else
+    {
+
+      console.log(match);
+      errors();
+
+    }
+}
